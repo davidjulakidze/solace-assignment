722a646db23c856972b27100b5b00ddaffe31f2f
David Julakidze <datoj81@gmail.com>
2025-05-08 12:31:00 -0400

    allow searching all columns

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,38 +1,63 @@
 import { NextRequest, NextResponse } from "next/server";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { ilike, or, count } from "drizzle-orm";
+import { ilike, or, count, sql } from "drizzle-orm";
 import { advocateLimit } from "@/db/seed/advocates";
 
 export async function GET(request: NextRequest) {
   const query = request.nextUrl.searchParams.get("query");
   const page = request.nextUrl.searchParams.get("page");
 
-  const filter = query
-    ? or(
-        ilike(advocates.firstName, `%${query}%`),
-        ilike(advocates.lastName, `%${query}%`),
-        ilike(advocates.city, `%${query}%`),
-        ilike(advocates.degree, `%${query}%`)
-      )
-    : undefined;
+  let filterForDb;
+
+  if (query && query.trim() !== "") {
+    const searchQuery = `%${query}%`;
+    const conditions = [
+      ilike(advocates.firstName, searchQuery),
+      ilike(advocates.lastName, searchQuery),
+      ilike(advocates.city, searchQuery),
+      ilike(advocates.degree, searchQuery),
+      sql`jsonb_typeof( (${advocates.specialties} #>> '{}')::jsonb ) = 'array' AND
+            EXISTS (
+                SELECT 1
+                FROM jsonb_array_elements_text( (${advocates.specialties} #>> '{}')::jsonb ) elem
+                WHERE elem.value ILIKE ${searchQuery}
+            )`,
+      sql`${advocates.yearsOfExperience}::text ILIKE ${searchQuery}`,
+      sql`${advocates.phoneNumber}::text ILIKE ${searchQuery}`,
+    ];
+    filterForDb = or(...conditions);
+  } else {
+    filterForDb = undefined;
+  }
 
   const dataPromise = db
     .select()
     .from(advocates)
-    .where(filter)
+    .where(filterForDb)
     .limit(advocateLimit)
     .offset(page ? (Number(page) - 1) * advocateLimit : 0);
 
   const totalCountPromise = db
     .select({ count: count() })
     .from(advocates)
-    .where(filter);
+    .where(filterForDb);
 
-  const [data, totalCount] = await Promise.all([
-    dataPromise,
-    totalCountPromise,
-  ]);
+  try {
+    const [data, totalCountResult] = await Promise.all([
+      dataPromise,
+      totalCountPromise,
+    ]);
 
-  return NextResponse.json({ data, totalCount: totalCount[0].count });
+    return NextResponse.json({
+      data,
+      totalCount: totalCountResult[0]?.count ?? 0,
+    });
+  } catch (error) {
+    console.error("Error fetching advocates:", error);
+    return NextResponse.json(
+      { message: "An error occurred while fetching data." },
+      { status: 500 }
+    );
+  }
 }
